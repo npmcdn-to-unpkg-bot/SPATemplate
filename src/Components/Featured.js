d57189e477c8ae9986ad1efa4d528c391be12846
Jai Singh Mankoo <jsmankoo@outlook.com>
2016-04-25 01:29:03 -0400

    Switched to Owl Carousel

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -1,301 +1,164 @@
 import React from 'react';
-import {Motion, spring, presets} from 'react-motion';
+import {connect} from 'react-redux';
 import MediaQuery from 'react-responsive';
-var Swipeable = require('react-swipeable')
 
-const Featured = ({children, Search, springSettings})=>(
-  <div className="Featured">
-    <MediaQuery maxDeviceWidth={767}>
-      <Mobile children={children} Search={Search} />
-    </MediaQuery>
-    <MediaQuery minDeviceWidth={768} maxDeviceWidth={1024}>
-      <Tablet children={children} Search={Search} />
-    </MediaQuery>
-    <MediaQuery minDeviceWidth={1025}>
-      <Desktop children={children} Search={Search} />
-    </MediaQuery>
-  </div>
-);
+import OwlCarousel from './OwlCarousel';
 
-const Mobile = React.createClass({
-  getInitialState(){
-    return {
-      slideIndex : 0
-    };
-  },
-  setSlide(index){
-    return ()=>{
-      this.setState({
-        slideIndex : index
-      });
-    };
-  },
-  NextSlide(){
-    const {slideIndex} = this.state;
-    const {children} = this.props;
-    const nextIndex = (slideIndex + 1) % (children.length);
-    this.setState({
-      slideIndex: nextIndex
-    });
-  },
-  PrevSlide(){
-    const {slideIndex} = this.state;
-    const {children} = this.props;
-    const newIndex = (slideIndex - 1) % children.length < 0 ? 0: (slideIndex - 1) % children.length;
-    this.setState({
-      slideIndex: newIndex
-    });
-  },
-  componentDidMount(){
-    this.autoPlay = setInterval(()=>{
-      const {slideIndex} = this.state;
-      const {children} = this.props;
-      this.setState({
-        slideIndex: (slideIndex + 1) % (children.length)
-      })
-    }, 5000);
-  },
-  componentWillUnmount(){
-    clearInterval(this.autoPlay);
-  },
-  render(){
-    const {children, Search} = this.props;
-    const {slideIndex} = this.state;
-    return (
-      <div className='Mobile'>
-        <div className="Wrapper">
-          <div className="Headline">
-            <h2>FEATURED PROPERTIES</h2>
-          </div>
-          <div className="Properties">
+const Featured = ({Properties,Search})=>{
+  return (
+    <div className='Featured'>
+      <MediaQuery maxDeviceWidth={767}>
+        <Mobile
+            Properties={Properties}
+            Search={Search}
+            options={{
+              dots: true,
+              items: 2,
+              singleItem: true,
+              autoPlay: false,
+              navigation: false
+            }}
+          />
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={768} maxDeviceWidth={1024}>
+        <Tablet
+            Properties={Properties}
+            Search={Search}
+            options={{
+              dots: true,
+              items: 2,
+              items : 2,
+              itemsTablet : [1024,2],
+              autoPlay: false,
+              navigation: false
+            }}
+          />
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={1025}>
+        <Desktop
+            Properties={Properties}
+            Search={Search}
+            options={{
+              dots: true,
+              items: 3,
+              itemsTablet : [1200,3],
+              autoPlay: false,
+              navigation: false
+            }}
+          />
+      </MediaQuery>
+    </div>
+  );
+};
+
+const Mobile = ({Properties,Search,options})=>{
+  return (
+    <div className='Mobile'>
+      <div className='Wrapper'>
+        <div className='Headline'>
+          <h2>FEATURED PROPERTIES</h2>
+        </div>
+        <div className='Search'>
+          <a target='_blank' href='//google.com'>SEARCH THE MLS</a>
+        </div>
+        <div className='Properties'>
+          <OwlCarousel className='owl-carousel owl-theme' {...options}>
             {
-              !children ? <div /> :
-              children.map((child, index)=>(
-                <Swipeable
-                    onSwipingRight={this.PrevSlide}
-                    onSwipingLeft={this.NextSlide}
-                    key={index}
-                  >
-                  <Motion
-                      style={{
-                        x: spring((index-slideIndex)*100, presets.gentle),
-                      }}
-                    >
-                      {
-                        ({x})=>(
-                          <div className="Item" style={{
-                            transform: `translate(${x}%, 0%)`,
-                            width: `${100}%`,
-                            position:'absolute'
-                          }}>
-                            {child}
-                          </div>
-                        )
-                      }
-                  </Motion>
-                </Swipeable>
+              Properties.map(({Address, Link, Photo, Price}, index)=>(
+                <div className='item' key={index}>
+                  <a target='_blank' href={Link.value.url} className='Property'>
+                    <div className='Pic' style={{
+                        backgroundImage: `url('${Photo.value.main.url}')`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center'
+                      }}>
+                    </div>
+                    <div className='Address'>{Address.value}</div>
+                    <div className='Price'>{Price.value}</div>
+                  </a>
+                </div>
               ))
             }
-          </div>
-          <div className="Controls">
-            {
-              !children ? <div /> :
-              children.map((child, index)=>(
-                <i
-                    key={index}
-                    className={`fa fa-circle ${slideIndex === index ? 'active' : 'unactive'}`}
-                    onClick={this.setSlide(index)}
-                  />
-              ))
-            }
-          </div>
-          <div className="Search">
-            <a target='_blank' href={Search}>Search The MLS</a>
-          </div>
+          </OwlCarousel>
         </div>
       </div>
-    );
-  }
-});
+    </div>
+  );
+};
 
-const Tablet = React.createClass({
-  getInitialState(){
-    return {
-      slideIndex : 0
-    };
-  },
-  setSlide(index){
-    return ()=>{
-      this.setState({
-        slideIndex : index
-      });
-    };
-  },
-  NextSlide(){
-    const {slideIndex} = this.state;
-    const {children} = this.props;
-    const nextIndex = (slideIndex + 1) % (children.length - 1);
-    console.log('next' + nextIndex);
-    this.setState({
-      slideIndex: nextIndex
-    });
-  },
-  PrevSlide(){
-    const {slideIndex} = this.state;
-    const {children} = this.props;
-    const newIndex = (slideIndex + 1) % (children.length - 1) < 0 ? 0: (slideIndex + 1) % (children.length - 1);
-    console.log('prev' + newIndex);
-    this.setState({
-      slideIndex: newIndex
-    });
-  },
-  componentDidMount(){
-    this.autoPlay = setInterval(()=>{
-      const {slideIndex} = this.state;
-      const {children} = this.props;
-      this.setState({
-        slideIndex: (slideIndex + 1) % (children.length - 1)
-      })
-    }, 5000);
-  },
-  componentWillUnmount(){
-    clearInterval(this.autoPlay);
-  },
-  render(){
-    const {children, Search} = this.props;
-    const {slideIndex} = this.state;
-    return (
-      <div className='Tablet'>
-        <div className="Wrapper">
-          <div className="Headline">
-            <h2>FEATURED PROPERTIES</h2>
-            <a target='_blank' href={Search}>SEARCH THE MLS</a>
-          </div>
-          <div className="Properties">
+const Tablet = ({Properties,Search,options})=>{
+  return (
+    <div className='Tablet'>
+      <div className='Wrapper'>
+        <div className='Headline'>
+          <h2>FEATURED PROPERTIES</h2>
+          <a target='_blank' href={Search}>SEARCH THE MLS</a>
+        </div>
+        <div className='Properties'>
+          <OwlCarousel className='owl-carousel owl-theme' {...options}>
             {
-              !children ? <div /> :
-              children.map((child, index)=>(
-                <Swipeable
-                    onSwipingLeft={this.NextSlide}
-                    onSwipingRight={this.PrevSlide}
-                    key={index}
-                  >
-                  <Motion
-                      style={{
-                        x: spring((index-slideIndex)*100, presets.gentle),
-                      }}
-                    >
-                      {
-                        ({x})=>(
-                          <div className="Item" style={{
-                            transform: `translate(${x}%, 0%)`,
-                            width: `${100/2}%`,
-                            position:'absolute'
-                          }}>
-                            {child}
-                          </div>
-                        )
-                      }
-                  </Motion>
-                </Swipeable>
+              Properties.map(({Address, Link, Photo, Price}, index)=>(
+                <div className='item' key={index}>
+                  <a target='_blank' href={Link.value.url} className='Property'>
+                    <div className='Pic' style={{
+                        backgroundImage: `url('${Photo.value.main.url}')`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center'
+                      }}>
+                    </div>
+                    <div className='Address'>{Address.value}</div>
+                    <div className='Price'>{Price.value}</div>
+                  </a>
+                </div>
               ))
             }
-          </div>
-          <div className="Controls">
-            {
-              !children ? <div /> :
-              children.slice(1).map((child, index)=>(
-                <i
-                    key={index}
-                    className={`fa fa-circle ${slideIndex === index ? 'active' : 'unactive'}`}
-                    onClick={this.setSlide(index)}
-                  />
-              ))
-            }
-          </div>
+          </OwlCarousel>
         </div>
       </div>
-    );
-  }
-});
+    </div>
+  );
+};
 
-const Desktop = React.createClass({
-  getInitialState(){
-    return {
-      slideIndex : 0
-    };
-  },
-  setSlide(index){
-    return ()=>{
-      this.setState({
-        slideIndex : index
-      });
-    };
-  },
-  componentDidMount(){
-    this.autoPlay = setInterval(()=>{
-      const {slideIndex} = this.state;
-      const {children} = this.props;
-      this.setState({
-        slideIndex: (slideIndex + 1) % (children.length - 2)
-      })
-    }, 5000);
-  },
-  componentWillUnmount(){
-    clearInterval(this.autoPlay);
-  },
-  render(){
-    const {children, Search} = this.props;
-    const {slideIndex} = this.state;
-    return (
-      <div className='Desktop'>
-        <div className="Wrapper">
-          <div className="Headline">
-            <h2>FEATURED PROPERTIES</h2>
-            <a target='_blank' href={Search}>Search The MLS</a>
-          </div>
-          <div className="Properties">
+const Desktop = ({Properties,Search,options})=>{
+  return (
+    <div className='Desktop'>
+      <div className='Wrapper'>
+        <div className='Headline'>
+          <h2>FEATURED PROPERTIES</h2>
+          <a target='_blank' href={Search}>SEARCH THE MLS</a>
+        </div>
+        <div className='Properties'>
+          <OwlCarousel className='owl-carousel owl-theme' {...options}>
             {
-              !children ? <div /> :
-              children.map((child, index)=>(
-                <Motion
-                    key={index}
-                    style={{
-                      x: spring((index-slideIndex)*100, presets.gentle),
-                    }}
-                  >
-                    {
-                      ({x})=>(
-                        <div className="Item" style={{
-                          transform: `translate(${x}%, 0%)`,
-                          width: `${100/3}%`,
-                          position:'absolute'
-                        }}>
-                          {child}
-                        </div>
-                      )
-                    }
-                </Motion>
+              Properties.map(({Address, Link, Photo, Price}, index)=>(
+                <div className='item' key={index}>
+                  <a target='_blank' href={Link.value.url} className='Property'>
+                    <div className='Pic' style={{
+                        backgroundImage: `url('${Photo.value.main.url}')`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center'
+                      }}>
+                    </div>
+                    <div className='Address'>{Address.value}</div>
+                    <div className='Price'>{Price.value}</div>
+                  </a>
+                </div>
               ))
             }
-          </div>
-          <div className="Controls">
-            {
-              !children ? <div /> :
-              children.slice(2).map((child, index)=>(
-                <i
-                    key={index}
-                    className={`fa fa-circle ${slideIndex === index ? 'active' : 'unactive'}`}
-                    onClick={this.setSlide(index)}
-                  />
-              ))
-            }
-          </div>
+          </OwlCarousel>
         </div>
       </div>
-    );
-  }
-});
+    </div>
+  );
+};
 
 
-export default Featured;
+const mapStateToProps = ({Featured})=>{
+  console.log(Featured.toJS());
+  return {
+    Properties: Featured.get('Properties').toJS(),
+    Search: Featured.get('Search')
+  };
+};
+
+export default connect(mapStateToProps)(Featured);
